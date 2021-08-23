419a8107a1f3a84da82f78f83dfce8b9b1f82909
Lucas Andrade <lucas.chagas@opuspacind.onmicrosoft.com>
2021-08-23 16:42:57 -0300

    Feat: Adicionado o Identity para registrar e login.

---
+++ ApiRestCore-Camadas-master/Controllers/AuthController.cs
@@ -0,0 +1,84 @@
+﻿using ApiRestCore.Business.Interfaces;
+using ApiRestCore.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestCore.Controllers
+{
+    [Route("api")]
+    public class AuthController : MainController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger _logger;
+
+
+        public AuthController(INotificador notificador, 
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager,
+                              ILogger<AuthController> logger) : base(notificador)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _logger = logger;
+
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password); // criptografa e compara com hash que existe na base de dados
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false); // false significa que n guarda nenhuma informação para facilitar o login na proxima vez
+                return CustomResponse(registerUser);
+            }
+            foreach (var error in result.Errors)
+            {
+                NotificarErro(error.Description);
+            }
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("entrar")]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true); 
+            // true significa que pode travar o usuario depois de 5 tentativas erradas, e libera depois de x tempo(pode ser definido)
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("Usuario " + loginUser.Email + " logado com sucesso");
+                return CustomResponse(loginUser);
+            }
+            if (result.IsLockedOut)
+            {
+                NotificarErro("Usuário temporariamente bloqueado por tentativas inválidas");
+                return CustomResponse(loginUser);
+            }
+
+            NotificarErro("Usuário ou Senha incorretos");
+            return CustomResponse(loginUser);
+        }
+
+
+
+    }
+}
