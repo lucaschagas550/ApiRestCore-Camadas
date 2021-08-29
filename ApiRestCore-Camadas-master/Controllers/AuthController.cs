477083e75f31ee62754cd75fe90e10b261a2a885
Lucas Andrade <lucasandrade595@gmail.com>
2021-08-29 14:14:46 -0300

    Feat: Retornando mais informações ao requisitar o token.

--- ApiRestCore-Camadas-master/Controllers/AuthController.cs
+++ ApiRestCore-Camadas-master/Controllers/AuthController.cs
@@ -87,7 +87,7 @@ namespace ApiRestCore.Controllers
         }
 
 
-        private async Task<string> GerarJwt(string email)
+        private async Task<LoginResponseViewModel> GerarJwt(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -119,19 +119,19 @@ namespace ApiRestCore.Controllers
 
             var encodedToken = tokenHandler.WriteToken(token);
 
-            //var response = new LoginResponseViewModel
-            //{
-            //    AccessToken = encodedToken,
-            //    ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
-            //    UserToken = new UserTokenViewModel
-            //    {
-            //        Id = user.Id,
-            //        Email = user.Email,
-            //        Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
-            //    }
-            //};
+            var response = new LoginResponseViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UserToken = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
 
-            return encodedToken;
+            return response;
         }
 
         private static long ToUnixEpochDate(DateTime date)
