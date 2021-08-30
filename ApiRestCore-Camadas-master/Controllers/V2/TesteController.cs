fa5e5420867b7327f7838c7aa0ea32bf955a92e4
Lucas Andrade <lucas.chagas@opuspacind.onmicrosoft.com>
2021-08-30 15:21:25 -0300

    Feat: Adicionando Log.

--- ApiRestCore-Camadas-master/Controllers/V2/TesteController.cs
+++ ApiRestCore-Camadas-master/Controllers/V2/TesteController.cs
@@ -11,13 +11,33 @@ namespace ApiRestCore.Controllers.V2
     {
         private readonly ILogger _logger;
 
-        public TesteController(INotificador notificador, IUser appUser) : base(notificador, appUser)
+        public TesteController(INotificador notificador, IUser appUser, ILogger<TesteController> logger) : base(notificador, appUser)
         {
+            _logger = logger;
         }
 
         [HttpGet]
         public string Valor()
         {
+
+            //throw new Exception("Error");
+
+            //try
+            //{
+            //    var i = 0;
+            //    var result = 42 / i;
+            //}
+            //catch (DivideByZeroException e)
+            //{
+            //    e.Ship(HttpContext);
+            //}
+
+            _logger.LogTrace("Log de Trace");
+            _logger.LogDebug("Log de Debug");
+            _logger.LogInformation("Log de Informação");
+            _logger.LogWarning("Log de Aviso");
+            _logger.LogError("Log de Erro");
+            _logger.LogCritical("Log de Problema Critico");
 
             return "Sou a V2";
         }
